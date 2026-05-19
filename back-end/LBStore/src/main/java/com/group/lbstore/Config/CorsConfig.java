0dcaa9f447ea0b48ce0da4b0dbda2c077116d4fb
Baowsp <xuanbacvn2004@gmail.com>
2026-05-19 22:01:54 +0700

    deploy 2

--- back-end/LBStore/src/main/java/com/group/lbstore/Config/CorsConfig.java
+++ back-end/LBStore/src/main/java/com/group/lbstore/Config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Áp dụng cho mọi API (VD: /api/products, /api/v1/banners)
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000") // Cấp quyền cho origin Frontend React (Vite/CRA)
+                .allowedOriginPatterns("*") // Cho phép tất cả các nguồn (bao gồm cả các trang Vercel deploy, mobile và IP)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // Cho phép các hành động HTTP
                 .allowedHeaders("*") 
                 .allowCredentials(true); // Cho phép gửi kèm cookie/token
